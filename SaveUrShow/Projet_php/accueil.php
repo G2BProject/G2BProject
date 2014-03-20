9ab121d07f35d9ddbaadf5a73b4c6bb9f1a17d21
Aude Buch <aude.buchmuller@free.fr>
2014-03-20 09:41:36 +0100

    Ajout code php

---
+++ SaveUrShow/Projet_php/accueil.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Save ur show</title>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="style_menu.css" type="text/css" media="screen" />
+
+</head>
+
+<?php include("menu.php"); ?>
+
+<body>
+	<div class="Actus"><a href="#"> </a>
+		<div class="texte1"> <span class="a2">Justin Timberlake sort son nouveau single! </span></div>
+	</div>
+	<div class="Top3"><span class="a3">TOP 3 DES ARTISTES</span>
+		<div class="texte2"></div>
+	</div>
+	<div class="Concerts"><span class="a3"> CONCERTS A VENIR</span>
+		<div class="texte3"></div>
+	</div>
+	<div class="Coeur"><span class="a3">COUPS DE COEUR</span>
+		<div class="texte4"></div>
+	</div>
+	<div class="News"><span class="a3">NOUVEAUX ARTISTES</span>
+		<div class="texte5"></div>
+	</div>
+
+
+</body>
+</html>
