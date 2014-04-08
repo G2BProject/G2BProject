3efc143943b913112b078fcca9e94766e71af15a
Tplanes <t.planes.raisenauer@gmail.com>
2014-04-08 18:31:35 +0200

    MàJ formulaire

--- SaveUrShow/Projet_php/traitement_inscription.php
+++ SaveUrShow/Projet_php/traitement_inscription.php
@@ -8,15 +8,30 @@ catch (Exception $e)
         die('Erreur : ' . $e->getMessage());
 }
 
-$req = $bdd->prepare('INSERT INTO membre(pseudo, adresse_email, mot_de_passe, name, prenom, sexe, adresse) VALUES(?, ?, ?, ?, ?, ?, ?)');
+$Role_ID = $_POST['Role_ID'];
+$pseudo = $_POST['pseudo'];
+$adresse_email = $_POST['adresse_email'];
+$mot_de_passe_hache = sha1($_POST['mot_de_passe']);
+$name = $_POST['name'];
+$prenom = $_POST['prenom'];
+$sexe = $_POST['sexe'];
+$date_de_naissance = $_POST['an']."-".$_POST['mois']."-".$_POST['jour'];
+$adresse = $_POST['adresse'];
+
+var_dump($date_de_naissance);
+die();
+
+$req = $bdd->prepare('INSERT INTO membre(Role_ID, pseudo, adresse_email, mot_de_passe, name, prenom, sexe, date_de_naissance, adresse, date_inscription) VALUES(:Role_ID, :pseudo, :adresse_email, :mot_de_passe, :name, :prenom, :sexe, :date_de_naissance, :adresse, NOW())');
 $req -> execute(array(
-	$_POST['pseudo'],
-	$_POST['adresse_email'],
-	$_POST['mot_de_passe'],
-	$_POST['name'],
-	$_POST['prenom'],
-	$_POST['sexe'],
-	$_POST['adresse'],
+	'Role_ID' => $Role_ID,
+	'pseudo' => $pseudo,
+	'adresse_email' => $adresse_email,
+	'mot_de_passe' => $mot_de_passe_hache,
+	'name' => $name,
+	'prenom' => $prenom,
+	'sexe' => $sexe,
+	'date_de_naissance' => $date_de_naissance,
+	'adresse' => $adresse,
 	));
 
 header('Location: Squelette.php');
