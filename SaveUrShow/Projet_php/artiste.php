3c9f1782cbf8e814a90b99940b753fd3f5e6bde7
Aude Buch <aude.buchmuller@free.fr>
2014-04-11 13:55:58 +0200

    ajout pages artiste et concert

--- SaveUrShow/Projet_php/artiste.php
+++ SaveUrShow/Projet_php/artiste.php
@@ -3,8 +3,8 @@
 
 <head>
 <meta charset="UTF-8">
-<title>Concerts</title>
-<link rel="stylesheet" href="style_concert.css" type="text/css" media="screen" />
+<title>Artistes</title>
+<link rel="stylesheet" href="style_artiste.css" type="text/css" media="screen" />
 </head>
 
 <body>
@@ -17,38 +17,37 @@
 Nom de l'artiste:
 </p>
 <ul>
-<li class="caract"><div class="txt2" href="#"><a class="style">Qui? </a>   Nom du ou des artistes se produisant sous le nom indiqu��</div></li>
-<li class="caract"><div class="txt2"href="#"><a class="style">Date </a>:   le JJ/MM/AAAA �� XXhXX</div></li>
-<li class="caract"><div class="txt2"href="#"><a class="style">Lieu</a>:   Adresse pr��cise ou lien vers la page de la salle correspondante si r��pertori��e</div></li>
-<li class="caract"><div class="txt2"href="#"><a class="style">   O�� r��server</a>:    Liens vers un site vendant des places</div></li>
-<li class="caract"><div class="txt2"href="#"><a class="style">   Nombre de places</a>:    XXXX</div></li>
-<div><a href="#"><img class="loc" src="Localiser.jpg"/></a></div>
-<div class="localiser">Localiser</div>
+<li class="caract"><div class="txt2" href="#"><a class="style">Qui? </a>   Nom du ou des artistes se produisant sous le nom indiqué</div></li>
+<li class="caract"><div class="txt2"href="#"><a class="style">Style </a>:   Type de musique jouée, caractéristiques musicales de l'artiste</div></li>
+<li class="caract"><div class="txt2"href="#"><a class="style">A venir </a>:   Date du prochain évènement impliquant l'artiste avec éventuellement le lien y renvoyant</div></li>
+<li class="caract"><div class="txt2"href="#"><a class="style">   Dernière production </a>:    Nom et date de la dernière production de l'artiste</div></li>
+<li class="caract"><div class="txt2"href="#"><a class="style">   Nombre de fans</a>:    XXX</div></li>
+<div class="fan">Devenir fan</div>
 </ul>
 </div>
 
 </div>
 
 <div class="container3">
-<div><a href="#"><img class="barre" src="barre.jpg"/></a></div>
-<div><a href="#"><img class="liste" src="liste.jpg"/></a></div>
-<div class=mail>Recevoir l'actualit�� de WOODKID par mail</div>
+<div id="barre"><a href="#"><img class="barre" /></a></div>
+<div id="liste"><a href="#"><img class="liste" /></a></div>
+<div class=mail>Recevoir l'actualité de WOODKID par mail</div>
 </div>
 
 <div class="container2">
-<div><a class="txt3" href="#">Description du concert</a></div>
+<div><a class="txt3" href="#">BIOGRAPHIE</a></div>
 <div><a class="txtbio" href="#">Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla </a></div>
 <div><a class="txtbio" href="#">Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla Blabla </a></div>
 </div>
 
 <div class="container4">
 <ul>
-<div><a class="txt6" href="#">Messages post��s sur ce concert</a></div>
+<div><a class="txt6" href="#">Messages postés sur cet artiste</a></div>
 </ul>
 </div>
 
 <div class="container5">
-<div><a href="#"><img class="user" src="user.jpg"/></a></div>
+<div><a href="#"><img class="user" /></a></div>
 <div><a class="txt4" href="#">Nom de l'utilisateur</a></div>
 <div><a class="txt4" href="#">Date</a></div>
 <div class="formulaire">
@@ -57,13 +56,13 @@ Nom de l'artiste:
 </div>
 
 <div class="container6">
-<div><a href="#"><img class="user2" src="user.jpg"/></a></div>
+<div><a href="#"><img class="user2" /></a></div>
 <div><a class="txt7" href="#">Bidule1</a></div>
 <div><a class="txt8" href="#">Date du post</a></div>
 <div><a class="txt8" href="#">Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post</a></div>
 <br>
 </br>
-<div><a href="#"><img class="user2" src="user.jpg"/></a></div>
+<div><a href="#"><img class="user2"></a></div>
 <div><a class="txt7" href="#">Bidule2</a></div>
 <div><a class="txt8" href="#">Date du post</a></div>
 <div><a class="txt8" href="#">Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post Post</a></div>
