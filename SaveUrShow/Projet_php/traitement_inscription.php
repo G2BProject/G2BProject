0ba3dabf16547fc43b2b96f0d7662e87e7662e8f
Tplanes <t.planes.raisenauer@gmail.com>
2014-04-11 13:39:40 +0200

    Ajout des fonctions confirmations mdp et mail

--- SaveUrShow/Projet_php/traitement_inscription.php
+++ SaveUrShow/Projet_php/traitement_inscription.php
@@ -9,8 +9,35 @@ catch (Exception $e)
 }
 
 $Role_ID = $_POST['Role_ID'];
-$pseudo = $_POST['pseudo'];
-$adresse_email = $_POST['adresse_email'];
+
+if(preg_match("#\w#", $_POST['pseudo']))
+{
+	$pseudo = $_POST['pseudo'];
+}
+else
+{
+	die('Vous devez choisir un nom d\'utilisateur');
+}
+
+$adresse_email2 = $_POST['emailConfirm'];
+
+if(preg_match("#^[a-zA-Z0-9_.]+@\w{2,}\.[a-z]{2,4}$#", $_POST['adresse_email']))
+{
+	if(preg_match("#$adresse_email2#", $_POST['adresse_email']))
+	{
+		$adresse_email = $_POST['adresse_email'];	
+	}
+	else
+	{
+		die('Les deux adresses mails renseignées doivent être identiques');
+	}
+}
+else
+{
+	die('Votre adresse mail n\'a pas un format valide.');
+}
+
+$mot_de_passe = $_POST['mot_de_passe'];
 
 if(preg_match("#[0-9]+#", $_POST['mot_de_passe']))
 {
@@ -20,27 +47,35 @@ if(preg_match("#[0-9]+#", $_POST['mot_de_passe']))
 		{
 			if(preg_match("#\w{8,}+#", $_POST['mot_de_passe']))
 			{
-				$mot_de_passe_hache = $_POST['mot_de_passe'];
+				if(preg_match("#$mot_de_passe#", $_POST['passwordConfirm']))
+				{
+					$mot_de_passe_hache = sha1($_POST['mot_de_passe']);	
+				}
+				else
+				{
+					die('Les deux mots de passe renseignés doivent être identiques');
+				}
 			}
 			else
 			{
-				die('Votre mot de passe est nul.');
+				die('Votre mot de passe doit contenir au moins un chiffre.');
 			}
 		}
 		else
 		{
-			die('Votre mot de passe est nul.');
+			die('Votre mot de passe doit contenir au moins une lettre majuscule.');
 		}
 	}
 	else
 	{
-		die('Votre mot de passe est nul.');
+		die('Votre mot de passe doit contenir au moins une lettre minuscule.');
 	}
 }
 else
 {
-	die('Votre mot de passe est nul.');
+	die('Votre mot de passe doit comporter au moins 8 caractères.');
 }
+
 
 $name = $_POST['name'];
 $prenom = $_POST['prenom'];
