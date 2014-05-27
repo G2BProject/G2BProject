090b55bd43501b72517f75ae694e467047799558
Julian <julcity77@gmail.com>
2014-05-27 17:36:47 +0200

    ajout des salles qui s'affichent par departement

--- mvc1/controleurs/listeSalles.php
+++ mvc1/controleurs/listeSalles.php
@@ -1,11 +1,17 @@
-	<?php 
-	include('modeles/modele_salle.php');
-	$reqSalles=listSalles();
+<?php 
+include('modeles/modele_salle.php');
+if(empty($_POST['departement'])){
+	//$reqSalles=listSalles();
 	if(isset($_SESSION['pseudo'])){
 		$reqSallesDep= listSallesDep($_SESSION['departement']);
 	}
 	include('vues/header.php');
 	include('vues/vue_salles.php');
-
 	include('vues/footer.php');
-	?>
+	}else{
+	include('vues/header.php');
+	$reqSallesDepP= listSallesDep($_POST['departement']);
+	include('vues/vue_salles.php');
+	include('vues/footer.php');
+	}
+?>
--- mvc1/vues/vue_salles.php
+++ mvc1/vues/vue_salles.php
@@ -1,6 +1,14 @@
 ﻿<?php
-	if(isset($_SESSION['pseudo'])){
-		echo'<p > Voici les salles de votre région!<br /><br /><br /><br /></p>';
+	if(isset($_SESSION['pseudo']) and empty($reqSallesDepP)){
+		echo'<div class="listDep"><p> Voici les salles de votre région : '.$_SESSION['departement'].'<br /><br /></p>';
+		echo'
+			<p >Si vous voulez voir les salles d\'un autre departement, rentrez le ici : </p>
+			<form action="index.php?page=listeSalles" method="post">
+				<p><label >Departement :</label><input type="text" name="departement" id="departement"/> ex: pour Paris tapez:75</p>
+				<p ><input type="submit" value="Voir les salles de ce departement"></p>
+       		</form>
+       		</div>
+		';
 		foreach ($reqSallesDep as $salle){
 			echo'
 			<div class="conteneur"> <img class="gauche" alt="Concert" width="200" height="200" src="ressources/image/Salle1.png" />
@@ -10,13 +18,22 @@
 			';
 		}
 	}else{
-		foreach ($reqSalles as $salle){
-			echo'
-			<div class="conteneur"> <img class="gauche" alt="Concert" width="200" height="200" src="ressources/image/Salle1.png" />
-			  <h1>nom de la salle: '.$salle['nom_de_la_salle'].'</h1> <p> adresse: '.$salle['adresse_salle'].'&nbsp; numero: '.$salle['numero_de_telephone'].'  </p><h2> Concert à venir: <p><a href="Cascadeur.html">Cascadeur,</a> <a href="Fauve.html">Fauve,</a> <a href="La Maison Tellier.html">La Maison Tellier</a></p> </h2>
-
-			</div>
+		echo'<div class="listDep">
+			<p >Si vous voulez voir les salles d\'un departement précis, rentrez le ici : </p>
+			<form action="index.php?page=listeSalles" method="post">
+				<p><label>Departement :</label><input type="text" name="departement" id="departement"/> ex: pour Paris tapez:75</p>
+				<p><input type="submit" value="Voir les salles de ce departement"></p>
+       		</form>
+       		</div>
+		';
+		if(!empty($reqSallesDepP)){
+			foreach ($reqSallesDepP as $salle){
+				echo'
+				<div class="conteneur"> <img class="gauche" alt="Concert" width="200" height="200" src="ressources/image/Salle1.png" />
+				<h1>nom de la salle: '.$salle['nom_de_la_salle'].'</h1> <p> adresse: '.$salle['adresse_salle'].'&nbsp; numero: '.$salle['numero_de_telephone'].'  </p><h2> Concert à venir: <p><a href="Cascadeur.html">Cascadeur,</a> <a href="Fauve.html">Fauve,</a> <a href="La Maison Tellier.html">La Maison Tellier</a></p> </h2>
+				</div>
 			';
+			}
 		}
-}
+	}
 ?>
