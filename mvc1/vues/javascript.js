7188abc7569c4a10d6e422364b2f9343e1a3a8a3
Julian <julcity77@gmail.com>
2014-05-27 18:07:04 +0200

    inscription avec un javascript parfait omg

--- mvc1/vues/javascript.js
+++ mvc1/vues/javascript.js
@@ -1,23 +1,50 @@
 
     function verifEmail() {
-    	var email = document.getElementById("adresse_email").value;
-    	var emailconf = document.getElementById("emailConfirm").value;
-    	if(email==emailconf){
-    		document.getElementById("adresse_email").style.backgroundColor="#33CC00";
-    		document.getElementById("emailConfirm").style.backgroundColor="#33CC00";
-    	}else{
+        var email = document.getElementById("adresse_email").value;
+        var emailconf = document.getElementById("emailConfirm").value;
+        var reg = new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
+
+        if(reg.test(email))
+        {
+            document.getElementById("adresse_email").style.backgroundColor="#33CC00";
+            document.getElementById("emailConfirm").style.backgroundColor="#33CC00";
+             if(email!=emailconf){
+                document.getElementById("adresse_email").style.backgroundColor="#990000";
+                document.getElementById("emailConfirm").style.backgroundColor="#990000";
+            }
+        }
+        else
+        {
             document.getElementById("adresse_email").style.backgroundColor="#990000";
             document.getElementById("emailConfirm").style.backgroundColor="#990000";
         }
+       
+
     }
 
     function verifPassword(){
     	var password = document.getElementById("mot_de_passe").value;
     	var passwordConfirm = document.getElementById("passwordConfirm").value;
-    	if(passwordConfirm==password){
-    		document.getElementById("passwordConfirm").style.backgroundColor="#33CC00"	;
-    		document.getElementById("mot_de_passe").style.backgroundColor="#33CC00"	;
-    	}else{
+        var OK = true;
+    	if(passwordConfirm!=password){
+    		OK = false;
+        }
+        re = /[0-9]/;
+        if(!re.test(password)) {
+            OK = false;
+        }
+        re = /[a-z]/;
+        if(!re.test(password)) {
+            OK = false;
+        }
+        re = /[A-Z]/;
+        if(!re.test(password)) {
+            OK = false;
+        }
+        if(OK){
+            document.getElementById("passwordConfirm").style.backgroundColor="#33CC00"  ;
+            document.getElementById("mot_de_passe").style.backgroundColor="#33CC00" ;
+        }else{
             document.getElementById("passwordConfirm").style.backgroundColor="#990000"  ;
             document.getElementById("mot_de_passe").style.backgroundColor="#990000" ;
         }
