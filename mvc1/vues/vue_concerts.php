b11207b74613ff049ecd5737d93761c5cde7e150
tplanes <t.planes.raisenauer@gmail.com>
2014-06-12 10:12:51 +0200

    Correctif sur l'ajout salle/concert.

--- mvc1/vues/vue_concerts.php
+++ mvc1/vues/vue_concerts.php
@@ -2,8 +2,6 @@
 	if(isset($_SESSION['pseudo']) and empty($reqConcertDep)){
 		echo'<div class="listDep"><p> '.$CONCERT.' : '.$_SESSION['departement'].' <br /><br /></p>';
 		echo'
-			<br />
-			<br /> 
 			<p >'.$_CONCERT_DEP.' :<br /><br /></p>
 			<form action="index.php?page=listeConcerts" method="post">
 				<p><label >'.$DEPARTEMENT.' :</label><input type="text" name="departement" id="departement"/> '.$TXT_EX.'</p>
@@ -14,7 +12,8 @@
 		foreach ($reqConcertDep as $concert){
 			echo'
 			<div class="conteneur"> <img class="gauche" alt="Concert" width="200" height="200" src="ressources/image/Salle1.png" />
-			  <h1>Nom du concert : '.$concert['nom_du_concert'].'</h1> <p> Date et heure : '.$concert['date_du_concert'].' à '.$concert['heure_du_concert'].' </p></div>';
+			  <h1>Nom du concert : '.$concert['nom_du_concert'].'</h1> <p> Date et heure : '.$concert['date_du_concert'].' à '.$concert['heure_du_concert'].' </p>
+			  </div>';
 		}
 	}else{
 		echo'<div class="listDep"><br />
@@ -29,7 +28,8 @@
 			foreach ($reqConcertDep as $concert){
 				echo'
 				<div class="conteneur"> <img class="gauche" alt="Concert" width="200" height="200" src="ressources/image/Salle1.png" />
-				<h1>Nom du concert : '.$concert['nom_du_concert'].'</h1> <p> Date et heure : '.$concert['date_du_concert'].' à '.$concert['heure_du_concert'].' </p><p> Salle : '.$concert['nom_de_la_salle'].'</div>';
+				<h1>Nom du concert : '.$concert['nom_du_concert'].'</h1> <p> Date et heure : '.$concert['date_du_concert'].' à '.$concert['heure_du_concert'].' </p><p> Salle : '.$concert['nom_de_la_salle'].'
+				</div>';
 			}
 		}
 	}
--- mvc1/vues/vue_salles.php
+++ mvc1/vues/vue_salles.php
@@ -4,18 +4,16 @@
 		echo'
 			<p > '.$_SALLES_DEP.': </p>
 			<form action="index.php?page=listeSalles" method="post">
-				<p><label >$DEPARTEMENT :</label><input type="text" name="departement" id="departement"/> $TXT_EX</p>
-				<p ><input type="submit" value="$_VOIR_SALLE"></p>
+				<p><label >'.$DEPARTEMENT.':</label><input type="text" name="departement" id="departement"/> $TXT_EX</p>
+				<p ><input type="submit" value="'.$_VOIR_SALLE.'"></p>
        		</form>
        		</div>
 		';
 		foreach ($reqSallesDep as $salle){
 			echo'
 			<div class="conteneur"> <img class="gauche" alt="Concert" width="200" height="200" src="ressources/image/Salle1.png" />
-			  <h1>nom de la salle: '.$salle['nom_de_la_salle'].'</h1> <p> adresse: '.$salle['adresse_salle'].'&nbsp; numero: '.$salle['numero_de_telephone'].'  </p><h2> Concert à venir: <p><a href="Cascadeur.html">Cascadeur,</a> <a href="Fauve.html">Fauve,</a> <a href="La Maison Tellier.html">La Maison Tellier</a></p> </h2>
-
-			</div>
-			';
+			  <h1>nom de la salle: '.$salle['nom_de_la_salle'].'</h1> <p> adresse: '.$salle['adresse_salle'].'&nbsp; numero: '.$salle['numero_de_telephone'].'
+			  </div>';
 		}
 	}else{
 		echo'<div class="listDep">
@@ -26,13 +24,12 @@
        		</form>
        		</div>
 		';
-		if(!empty($reqSallesDepP)){
-			foreach ($reqSallesDepP as $salle){
+		if(!empty($reqSallesDep)){
+			foreach ($reqSallesDep as $salle){
 				echo'
 				<div class="conteneur"> <img class="gauche" alt="Concert" width="200" height="200" src="ressources/image/Salle1.png" />
-				<h1>nom de la salle: '.$salle['nom_de_la_salle'].'</h1> <p> adresse: '.$salle['adresse_salle'].'&nbsp; numero: '.$salle['numero_de_telephone'].'  </p><h2> Concert à venir: <p><a href="Cascadeur.html">Cascadeur,</a> <a href="Fauve.html">Fauve,</a> <a href="La Maison Tellier.html">La Maison Tellier</a></p> </h2>
-				</div>
-			';
+				<h1>nom de la salle: '.$salle['nom_de_la_salle'].'</h1> <p> adresse: '.$salle['adresse_salle'].'&nbsp; numero: '.$salle['numero_de_telephone'].'
+				</div>';
 			}
 		}
 	}
