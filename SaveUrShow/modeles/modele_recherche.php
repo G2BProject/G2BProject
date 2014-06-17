252ba7bcbc99732a46944191faa5fbe4ff146824
Julcity <julcity77@gmail.com>
2014-06-17 20:48:11 +0200

    resultat recherche concert bonne affichage

--- SaveUrShow/modeles/modele_recherche.php
+++ SaveUrShow/modeles/modele_recherche.php
@@ -5,11 +5,15 @@
 			$req = $bdd -> query("SELECT nom_artiste FROM artiste WHERE nom_artiste like '%$mot_cle%'");
 			return $req;
 		}elseif($type_recherche == '2'){
-			$req = $bdd -> query("SELECT nom_du_concert FROM concert WHERE nom_du_concert like '%$mot_cle%'");
-			return $req;
+			// $req = $bdd -> query("SELECT nom_du_concert FROM concert WHERE nom_du_concert like '%$mot_cle%'");
+			// return $req;
+
+			$req = $bdd -> prepare('SELECT concert.nom_du_concert, concert.date_du_concert, concert.heure_du_concert, concert.image_concert, salle.nom_de_la_salle, artiste.nom_artiste FROM concert, salle, artiste, representation WHERE concert.salle_ID = salle.ID AND (concert.nom_du_concert like :mot_cle) AND representation.concert_ID=concert.ID and representation.artiste_ID=artiste.ID');
+			$req -> execute(array('mot_cle' => '%'.$mot_cle.'%'));
+			return $req ;
 		}else{
-			$req = $bdd -> query("SELECT nom_de_la_salle FROM salle WHERE nom_de_la_salle like '%$mot_cle%'");
-			return $req;
+				$req = $bdd -> query("SELECT nom_de_la_salle FROM salle WHERE nom_de_la_salle like '%$mot_cle%'");
+				return $req;
 		}
     			
 			}
--- SaveUrShow/vues/vue_resultat.php
+++ SaveUrShow/vues/vue_resultat.php
@@ -5,7 +5,7 @@ echo "<ul>";
 	if($type_recherche== '1'){
 		if($reqRecherche!='empty'){
 			echo "<br/>";
-		echo "<p class='result'>".$TXT_ARTISTES."</p>";
+		echo "<p class='result' style='color:#66CCCC;' >".$TXT_ARTISTES."</p>";
 		foreach ($reqRecherche as $nom_artiste) {
 				
 				echo '<li> <a href="?page=artiste&artiste='.$nom_artiste['nom_artiste'].'"> '.$nom_artiste['nom_artiste'].' </a><br /></li>';
@@ -15,15 +15,21 @@ echo "<ul>";
 	}elseif ($type_recherche=='2'){
 		echo "<br/>";
 		if($reqRecherche!='empty'){
-				echo "<p class='result'>".$TXT_CONCERTS."</p>";
-		foreach ($reqRecherche as $nom_du_concert) {
-				echo '<li> <a href=""> '.$nom_du_concert['nom_du_concert'].'</a><br /></li>';
+				echo "<p class='result' style='color:#66CCCC;'>".$TXT_CONCERTS."</p>";
+		foreach ($reqRecherche as $concert) {
+			echo'
+			<div class="conteneur"> <img class="gauche" alt="Concert" width="200" height="200" src="'.$concert['image_concert'].'" />
+			  <h1> '.$NOM_CONCERT.' : <a href="?page=concert&concert='.$concert['nom_du_concert'].'">'.$concert['nom_du_concert'].'</a></h1> 
+			  <p> '.$DATE.''.$ET.''.$HEURE_CONCERT.' : '.$concert['date_du_concert'].' à '.$concert['heure_du_concert'].'</p>
+			  <p> '.$NOM_SALLE.' : '.$concert['nom_de_la_salle'].'</p>
+			  <p>'.$NOM_ARTISTE.' : '.$concert['nom_artiste'].'</p>
+			  </div>';
 			}
 		}
 	}elseif ($type_recherche=='3'){
 		echo "<br/>";
 			if($reqRecherche!='empty'){
-				echo "<p class='result'>".$TXT_SALLES."</p>";
+				echo "<p class='result' style='color:#66CCCC;'>".$TXT_SALLES."</p>";
 		foreach ($reqRecherche as $nom_de_la_salle) {
 				echo '<li> <a href="">'.$nom_de_la_salle['nom_de_la_salle'].'</a><br /></li>';
 			}
