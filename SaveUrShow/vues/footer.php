e46a4e4a09ac202f49086eece6f333be684f2843
Julian <julcity77@gmail.com>
2014-06-17 13:41:12 +0200

    ajout de la faq

--- SaveUrShow/vues/footer.php
+++ SaveUrShow/vues/footer.php
@@ -2,7 +2,7 @@
 <footer>
 <div id="boutons_bas">
 	<ul>
-		<li class="boutons3"><a href="#"><?php echo $TXT_FAQ ?></a></li>
+		<li class="boutons3"><a href="index.php?page=faq"><?php echo $TXT_FAQ ?></a></li>
 		<li class="boutons3"><a href="index.php?page=contact"><?php echo $TXT_CONTACTS ?></a></li>
 		<li class="boutons3"><a href="index.php?page=cgu"><?php echo $TXT_CG ?></a></li>
 	</ul>
